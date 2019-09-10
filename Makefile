97d94b52abf8ad7c1715183224b7efd042f55c56
Awesome Toolboox <info@awesometoolbox.com>
2019-09-10 02:53:48 -0400

    Fix setup and README

--- Makefile
+++ Makefile
@@ -1,16 +1,3 @@
-SHELL := /bin/bash
-
-test:
-	PYTHONPATH=src pipenv run pytest
-
-white:
-	white src/ tests/
-
-update:
-	pipenv update --dev
-
-# clean
-
 clean: clean-build clean-pyc clean-test
 
 clean-build:
@@ -18,6 +5,7 @@ clean-build:
 	rm -fr dist/
 	rm -fr .eggs/
 	find . -name '*.egg-info' -exec rm -fr {} +
+	find . -name '*.egg' -exec rm -f {} +
 
 clean-pyc:
 	find . -name '*.pyc' -exec rm -f {} +
@@ -26,8 +14,13 @@ clean-pyc:
 	find . -name '__pycache__' -exec rm -fr {} +
 
 clean-test:
-	find . -name 'htmlcov' -exec rm -fr {} +
-	find . -name '.coverage' -exec rm -fr {} +
-	find . -name '.pytest_cache' -exec rm -fr {} +
-	find . -name '.cache' -exec rm -fr {} +
-	find . -name '.mypy_cache' -exec rm -fr {} +
+	rm -fr .cache
+	rm -fr .mypy_cache
+	rm -fr .pytest_cache
+	rm -f .coverage
+	rm -fr htmlcov/
+
+publish:
+	pipenv run python setup.py sdist
+	pipenv run python setup.py bdist_wheel --universal
+	twine upload dist/*
--- setup.py
+++ setup.py
@@ -1,22 +1,44 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+
 setup(
     name="otf2ttf",
-    version='0.1',
+    version="0.2",
     packages=find_packages("src/"),
-    package_dir={
-        '': 'src'
-    },
-    package_data={
-    },
+    package_dir={"": "src"},
+    package_data={},
     include_package_data=True,
-    entry_points={
-        "console_scripts": [
-            'otf2ttf=otf2ttf:main',
-        ],
-    },
-    install_requires=[
-        "fonttools",
-        "cu2qu",
-    ]
+    entry_points={"console_scripts": ["otf2ttf=otf2ttf:main"]},
+    install_requires=["fonttools", "cu2qu"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Awesome Toolbox",
+    author_email="info@awesometoolbox.com",
+    description="command-line tool for converting OTF to TTF",
+    license="MIT",
+    keywords="font convert otf ttf",
+    url="https://github.com/awesometoolbox/otf2ttf",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Text Processing :: Fonts",
+        "Topic :: Multimedia :: Graphics",
+        "Topic :: Multimedia :: Graphics :: Graphics Conversion",
+    ],
 )
